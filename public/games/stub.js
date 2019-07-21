36975899ecd040f340f7f4ac835659d6ac4d240d
Steven Liu <steeven.liu2@gmail.com>
2019-07-21 14:35:57 +1000

    core engine working?

---
+++ public/games/stub.js
@@ -0,0 +1,8 @@
+addGame("stub",undefined,function(){
+    this.start=function(){
+        console.log("I started something!");
+    };
+    this.render=function(){
+        console.log("I rendered something!");
+    }
+})

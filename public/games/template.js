3dfdc55f076448267db7c1963fe886e423ff70dc
Steven Liu <steeven.liu2@gmail.com>
2019-07-21 15:21:04 +1000

    it works now

--- public/games/template.js
+++ public/games/template.js
@@ -1,18 +1,19 @@
-registerGame("template",undefined,function(){
-    
-    let state;
-    let players;
-    
+registerGame("template", undefined, function () {
+
+    let state = {};
+    let players = [];
+
     let id;
-    
-    this.start = function(_div, _id, _players){
- 
+    let _div;
+    this.start = function (div, _id, _players) {
+        _div = div;
         players = _players;
         state.turn = 0;
-        
-        for (let i = 0; i < players; i++) state.score[i] = 0;
-        
-        _div.innerHTML = `
+        state.score = [];
+        for (let i = 0; i < players.length; i++) state.score[i] = 0;
+        let preinnerHTML = "";
+        id=players.indexOf(_id);
+        preinnerHTML = `
         <h2>Template Game</h2>
         
         <table id='table_Score'>
@@ -21,17 +22,16 @@ registerGame("template",undefined,function(){
                 <th>Player</th>
                 <th>Score</th>
             </tr>`;
-        
-        for (let i = 0; i < players.length(); i++) _div.innerHTML += `
+
+        for (let i = 0; i < players.length; i++) preinnerHTML += `
             
             <tr>
                 <td>` + players[i] + `</td>
                 <td id='td_Player` + i + `Score'>0</td>
             </tr>`;
-            
-        }
-        
-        _div.innerHTML += `
+
+
+        preinnerHTML += `
         
         </table>
         
@@ -39,51 +39,51 @@ registerGame("template",undefined,function(){
         
         <h3 id='txt_Turn'></h3>
         
-        <formdata id='group_Actions'>
+        <fieldset id='group_Actions'>
         
             <button id='btn_GetScore'>Increase score (+1/click).</button>
             
             <button id='btn_EndTurn'>End Turn</button>
             
-        </formdata>
+        </fieldset>
         
         `;
-        
-        _div.querySelector("#btn_GetScore").addEventListener("click",()=>{
-            
-            state.score[id] ++;
-            
+        _div.innerHTML = preinnerHTML;
+        _div.querySelector("#btn_GetScore").addEventListener("click", () => {
+
+            state.score[id]++;
+
         });
-        
-        _div.querySelector("#btn_EndTurn").addEventListener("click",()=>{
-           
-            state.turn = (state.turn + 1) % players.length();
+
+        _div.querySelector("#btn_EndTurn").addEventListener("click", () => {
+
+            state.turn = (state.turn + 1) % players.length;
             transmit(state);
-            
+
         });
-        
-        
+
+
         transmit(state);
-    
+
     }
 
-    this.render = function(_state){
-    
+    this.recieve = function (_state) {
+
         state = _state;
-    
-        for (let i = 0; i < players.length(); i++) _div.querySelector("#td_Player" + i + "Score").innerHTML = state.score[i];
-             
-        if (state.turn == id){
-            
+
+        for (let i = 0; i < players.length; i++) _div.querySelector("#td_Player" + i + "Score").innerHTML = state.score[i];
+
+        if (state.turn == id) {
+
             _div.querySelector("#txt_Turn").innerHTML = "Your turn!";
             _div.querySelector("#group_Actions").disabled = false;
-        
+
         } else {
-            
+
             _div.querySelector("#txt_Turn").innerHTML = players[state.turn] + " to play.";
             _div.querySelector("#group_Actions").disabled = true;
-            
+
         }
     }
-    
+
 });
